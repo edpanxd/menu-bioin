fcf3aad962eb5641ab04dfc461cadac0ae02a529
aguirreini.xd@gmail.com <77561763+edpanxd@users.noreply.github.com>
2022-06-08 10:56:55 -0500

    v.1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
+window.Swal = Swal;
 
 Rails.start()
 Turbolinks.start()
