8c9815f79a65c084ecf2a3f9df727ec852d30a7c
aguirreini.xd@gmail.com <77561763+edpanxd@users.noreply.github.com>
2023-11-10 16:32:11 -0600

    jueves y viernes

--- app/models/platillo.rb
+++ app/models/platillo.rb
@@ -1,8 +1,7 @@
 class Platillo < ApplicationRecord
 
   validates :comida, presence: true
-  validates :repartidor_1, presence: true
-  validates :repartidor_2, presence: true
+
   validates :fecha, presence: true
 
   validates :fecha, uniqueness: { scope: :fecha, message: 'nombre por dia' }
